55fa1826fe849d6c549d20eac443655ef058ad60
Glen-Tigera <glen@tigera.io>
2025-10-22 16:01:37 -0400

    Add setup script for ai workload scenario

--- scripts/setup_scale_ai_workload_cluster.sh
+++ scripts/setup_scale_ai_workload_cluster.sh
@@ -1,5 +1,5 @@
 ## BPF Setup for Simulation Scenario
-bz init profile -n scale-ai-workload-cluster --additionals "CLUSTER_NODES=5,CLUSTER_IMAGE=ubuntu-2404-lts-amd64,GOOGLE_MASTER_MACHINE_TYPE=n2-standard-8,GOOGLE_NODE_MACHINE_TYPE=n2-standard-4,GOOGLE_INFRA_MACHINE_TYPE=n2-standard-4,GOOGLE_RR_MACHINE_TYPE=n2-standard-2,GOOGLE_ETCD_MACHINE_TYPE=n2-standard-2,NUM_INFRA_NODES=3,CRC_PODS_PER_NODE=3000" --product calient --provisioner gcp-kubeadm --dataplane CalicoBPF --release-stream v3.22 --hashrelease true --installer operator --k8sversion stable-1
+bz init profile -n scale-ai-workload-cluster --additionals "CLUSTER_NODES=400,CLUSTER_IMAGE=ubuntu-2404-lts-amd64,GOOGLE_MASTER_MACHINE_TYPE=n2-standard-32,GOOGLE_NODE_MACHINE_TYPE=n2-standard-8,GOOGLE_INFRA_MACHINE_TYPE=n2-standard-4,GOOGLE_RR_MACHINE_TYPE=n2-standard-2,GOOGLE_ETCD_MACHINE_TYPE=n2-standard-2,NUM_INFRA_NODES=3,CRC_PODS_PER_NODE=0" --product calient --provisioner gcp-kubeadm --dataplane CalicoBPF --release-stream v3.22 --hashrelease true --installer operator --k8sversion stable-1
 
 bz provision
 
